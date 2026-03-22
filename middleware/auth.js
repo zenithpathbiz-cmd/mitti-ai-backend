df10878688f2929a8a0a51e1f9370096e95a7ae6
zenithpathbiz-cmd <zenithpathbiz@gmail.com>
2026-03-22 18:20:53 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,19 @@
-module.exports = {
-    auth: require('./path/to/auth/middleware')
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+    const token = req.headers['authorization']?.split(' ')[1];
+
+    if (!token) {
+        return res.status(403).send({ message: 'Token is required for authentication' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).send({ message: 'Invalid token' });
+        }
+        req.user = user;
+        next();
+    });
 };
+
+module.exports = { auth: authMiddleware };
